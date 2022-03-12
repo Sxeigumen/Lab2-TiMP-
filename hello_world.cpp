7bc0a9e91e82201295ab763f95746b1eb29899f6
Sxeigumen <krasnov619@yandex.ru>
2022-03-13 01:00:33 +0300

    Version new

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-cout << "Hello world!";
+string Name;
+cout<<"What is your name?";
+cin >> Name;
+cout << "Hello World from " << Name;
 return 0;
 }
