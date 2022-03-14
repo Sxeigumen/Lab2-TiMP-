4036722f86b3b91953a6243f34176affd725beba
Sxeigumen <krasnov619@yandex.ru>
2022-03-14 23:38:31 +0300

    change code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include<string>
-int main(){
-    std::string name; //Создаём строку
-    std::cout << "What is your name?" ; //Выводи текст
-    std::cin >> name; //Ввод строки и сохранение её в name
-    std::cout << "Hello World from " << name; //Вывод тескта и строки name
-    return 0;
+#include <string>
+int
+main()
+{
+  std::string name;                  //Создаём строку
+  std::cout << "What is your name?"; //Выводи текст
+  std::cin >> name; //Ввод строки и сохранение её в name
+  std::cout << "Hello World from " << name; //Вывод тескта и строки name
+  return 0;
 }
