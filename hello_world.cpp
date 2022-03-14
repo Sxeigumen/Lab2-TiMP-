6d0301ac03c12a12cbf14ecc7ab639f3be04cc1f
Sxeigumen <krasnov619@yandex.ru>
2022-03-14 22:37:53 +0300

    hello_world.cpp with comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include<string>
 int main(){
-std::string name;
-std::cout << "What is your name?" ;
-std::cin >> name;
-std::cout << "Hello World from " << name;
-return 0;
+    std::string name; //Создаём строку
+    std::cout << "What is your name?" ; //Выводи текст
+    std::cin >> name; //Ввод строки и сохранение её в name
+    std::cout << "Hello World from " << name; //Вывод тескта и строки name
+    return 0;
 }
